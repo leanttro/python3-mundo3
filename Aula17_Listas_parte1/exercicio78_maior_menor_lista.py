bbd77c22a1695af4b7e47607b3600a3fc65ff919
SeuNome <leandroandrade020797@gmail.com>
2025-05-13 12:21:34 -0300

    Create exercicio78_maior_menor_lista.py

---
+++ Aula17_Listas_parte1/exercicio78_maior_menor_lista.py
@@ -0,0 +1,11 @@
+
+
+
+"""valores = list()
+for cont in range(0,5):
+    valores.append(int(input(f"Digite um valor para posição {cont}: ")))
+maior = max(valores)
+menor = min(valores)
+print(f"Você digitou os valores: {valores}")
+print(f"O maior valor foi: {maior}, na posição {valores.index(maior)} ")
+print(f"O menor valor foi: {menor}, na posição {valores.index(menor)}")"""
